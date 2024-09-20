958c8b3248d227857985e98ee43431075f16b4de
Visal Hewage <visalsathsara55@gmail.com>
2024-09-20 13:26:41 +0530

    hhh
    tt

--- pets/frontend/src/pages/appointment/AppointmentItem.js
+++ pets/frontend/src/pages/appointment/AppointmentItem.js
@@ -1,27 +1,4 @@
-/*// src/pages/AppointmentItem.js
-import React from 'react';
-import './AppointmentItem.css';
 
-const AppointmentItem = ({ app }) => {
-  return (
-    <tr>
-      <td>{app.appId}</td>
-      <td>{app.petId}</td>
-      <td>{app.name}</td>
-      <td>{app.reason}</td>
-      <td>{app.doctor}</td>
-      <td>{app.date}</td>
-      
-      <td>
-        
-      <button className='delete-button'>Delete</button>
-        
-      </td>
-    </tr>
-  );
-};
-
-export default AppointmentItem;*/
 
 
 import React, { useState } from 'react';
